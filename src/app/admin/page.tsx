2f6bdc59301dbfdb208a20cc8f774ffcf0cd0d26
Niraj Modi <modiniraj1034@gmail.com>
2024-04-10 19:57:09 +0530

    Form Done

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,11 @@
 "use client";
+import { CldUploadButton } from "next-cloudinary";
 import React, { useState } from "react";
+import { IoIosAddCircle } from "react-icons/io";
+import { MdCancel } from "react-icons/md";
 
 type FormType = {
+	slug: string;
 	title: string;
 	description: string;
 	prepTime: number;
@@ -16,6 +20,7 @@ type FormType = {
 
 const RecipeForm = () => {
 	const [formData, setFormData] = useState<FormType>({
+		slug: "",
 		title: "",
 		description: "",
 		prepTime: 0,
@@ -24,7 +29,7 @@ const RecipeForm = () => {
 		tags: [],
 		instructions: [],
 		ingredients: [],
-		tools: [],
+		tools: [{ name: "", link: "" }],
 		image: "",
 	});
 
@@ -32,17 +37,30 @@ const RecipeForm = () => {
 		const { name, value } = e.target;
 		setFormData((prevData) => ({
 			...prevData,
-			[name]: name === "tags" ? value.split(",") : value,
+			[name]:
+				name === "tags" || name === "instructions" || name == "ingredients"
+					? value.split(",")
+					: value,
 		}));
 	};
 
-	const handleSubmit = (e: any) => {
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
 		console.log(formData); // You can handle the form data submission here
+		formData["slug"] = formData.title.split(" ").join("-").toLowerCase();
+		const res = await fetch("/api/create", {
+			method: "POST",
+			body: JSON.stringify(formData),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
+		const data = await res.json();
+		console.log(data);
 	};
 
 	return (
-		<div className="max-w-md mx-auto bg-white p-8 rounded-md shadow-md">
+		<div className="w-[800px] mx-auto bg-white p-8 rounded-md shadow-md">
 			<h2 className="text-2xl font-semibold mb-4">Add Recipe</h2>
 			<form onSubmit={handleSubmit} className="space-y-4">
 				<div>
@@ -183,22 +201,130 @@ const RecipeForm = () => {
 
 				<div>
 					<label htmlFor="tools" className="block text-sm font-medium mb-1">
-						Tools (comma-separated):
+						Tools:
 					</label>
-					<input
-						type="text"
-						id="tools"
-						name="tools"
-						value={formData.tools.map((tool) => tool.name).join(",")}
-						onChange={handleChange}
-						required
-						className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
-					/>
-				</div>
+					{formData.tools.map((tool, index) => (
+						<div key={index} className="flex items-end gap-4">
+							<div className=" grow">
+								<label
+									htmlFor="name"
+									className="block text-sm font-medium mb-1"
+								>
+									Name:
+								</label>
+								<input
+									type="text"
+									id="name"
+									name="name"
+									value={formData.tools[index].name}
+									onChange={(e) => {
+										const { name, value } = e.target;
+										setFormData((prevData) => ({
+											...prevData,
+											tools: prevData.tools.map((tool, i) => {
+												if (i === index) {
+													return {
+														...tool,
+														[name]: value,
+													};
+												}
+												return tool;
+											}),
+										}));
+									}}
+									required
+									className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+								/>
+							</div>
 
+							<div className=" grow">
+								<label
+									htmlFor="link"
+									className="block text-sm font-medium mb-1"
+								>
+									Link:
+								</label>
+								<input
+									type="text"
+									id="link"
+									name="link"
+									value={formData.tools[index].link}
+									onChange={(e) => {
+										const { name, value } = e.target;
+										setFormData((prevData) => ({
+											...prevData,
+											tools: prevData.tools.map((tool, i) => {
+												if (i === index) {
+													return {
+														...tool,
+														[name]: value,
+													};
+												}
+												return tool;
+											}),
+										}));
+									}}
+									required
+									className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:border-blue-500"
+								/>
+							</div>
+
+							{index < formData.tools.length - 1 ? (
+								<div>
+									<button
+										type="button"
+										className="text-red-500 hover:text-red-600 transition-colors duration-300 !bg-red-300"
+										onClick={() =>
+											setFormData((prevData) => ({
+												...prevData,
+												tools: prevData.tools.filter((_, i) => i !== index),
+											}))
+										}
+									>
+										<MdCancel />
+									</button>
+								</div>
+							) : (
+								<div>
+									<button
+										type="button"
+										className="text-blue-500 hover:text-blue-600 transition-colors duration-300 !bg-blue-300"
+										onClick={() =>
+											setFormData((prevData) => ({
+												...prevData,
+												tools: [...prevData.tools, { name: "", link: "" }],
+											}))
+										}
+									>
+										<IoIosAddCircle />
+									</button>
+								</div>
+							)}
+						</div>
+					))}
+				</div>
+				<div>
+					<div>
+						<CldUploadButton
+							uploadPreset={process.env.NEXT_PUBLIC_CLOUDINARY_PRESET_NAME}
+							onSuccess={(res) => {
+								const data = res.info;
+
+								setFormData((prevData) => ({
+									...prevData,
+									//@ts-ignore
+									image: data.secure_url,
+								}));
+							}}
+						>
+							<span>Upload</span>
+						</CldUploadButton>
+					</div>
+				</div>
 				<button
 					type="submit"
 					className="w-full bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition-colors duration-300"
+					onClick={handleSubmit}
 				>
 					Submit
 				</button>
