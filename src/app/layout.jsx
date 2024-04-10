b6c8dd9fb5bbd70104397fcbcf71d22eb2146c98
PCoder23 <pranavkjha111@gmail.com>
2024-04-10 16:00:57 +0530

    google ads

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { NavBar } from "@/components";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
+export const metadata = {
 	title: "Next-Recipe",
 	description: "Find your next recipe!",
 };
 
 export default function RootLayout({
 	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+}) {
 	return (
 		<html lang="en">
+			<head>
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8267694389286905"
+     crossorigin="anonymous"></script>
+			</head>
 			<body className={`${inter.className}`}>
 				<NavBar />
 				{children}
--- src/app/recipe/[slug]/layout.jsx
+++ src/app/recipe/[slug]/layout.jsx
@@ -25,6 +25,10 @@ export async function generateMetadata({ params }) {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8267694389286905"
+     crossorigin="anonymous"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
